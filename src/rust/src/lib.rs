be21a8a34a84c2be8c4666ba6c7a4c270e32169f
Ben Raymond <ben.raymond@aad.gov.au>
2024-07-04 14:57:32 +1000

    don't use metadata method to identify dirs

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,11 @@
 use extendr_api::prelude::*;
 
+// single threaded using walkdir. Presumably could also do this with jwalk using jwalk::Parallelism::Serial if n_threads == 1
 #[extendr]
 fn walkdir(dir: String, include_dirs: bool) -> Vec<String> {
     walkdir::WalkDir::new(dir).follow_links(true).into_iter().filter_map(|e| e.ok())
         .map(|e| {
-            if include_dirs || !e.metadata().unwrap().is_dir() {
+            if include_dirs || !e.file_type().is_dir() {
                 e.into_path().into_os_string().into_string().unwrap()
             } else {
                 "".to_string()
@@ -18,7 +19,7 @@ fn walkdirp(dir: String, include_dirs: bool, n_threads: usize) -> Vec<String> {
     let prl = jwalk::Parallelism::RayonNewPool(n_threads);
     jwalk::WalkDir::new(dir).follow_links(true).skip_hidden(false).parallelism(prl).into_iter()
         .filter_map(|e| e.ok()).map(|e| {
-            if include_dirs || !e.metadata().unwrap().is_dir() { // including dirs or it's not a dir
+            if include_dirs || !e.file_type().is_dir() { // including dirs or it's not a dir
                 e.path().to_str().unwrap().to_string()
             } else {
                 "".to_string()
